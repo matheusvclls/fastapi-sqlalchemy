2b3845f8dad99adb9200920888e876ad54268702
Matheus <matheusvclls97@gmail.com>
2023-01-11 20:55:46 -0300

    feat: add users routes to the main app

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
+from api import users
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+app.include_router(users.router)
 
