fd2b87082dc3c282010a31bdeaf16401c08f366a
Matheus <matheusvclls97@gmail.com>
2023-01-12 10:33:47 -0300

    feat: add functions to the users domain

--- crud/users.py
+++ crud/users.py
@@ -1,48 +1,47 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
-from models.course import Course
-from schemas.course import CourseCreate, CourseUpdate
+from models.user import User
+from schemas.user import UserCreate,UserUpdate
 
-
-async def get_course(db: Session, course_id: int):
-    return db.query(Course).filter(Course.id == course_id).first()
-
-
-def get_courses(db: Session):
-    return db.query(Course).all()
-
-
-def get_user_courses(db: Session, user_id: int):
-    courses = db.query(Course).filter(Course.user_id == user_id).all()
-    return courses
+async def get_user(db: AsyncSession, user_id: int):
+    query = select(User).where(User.id == user_id)
+    result = db.execute(query)
+    return result.scalar_one_or_none()
 
 
-def create_course(db: Session, course: CourseCreate):
-    db_course = Course(
-        title=course.title,
-        description=course.description,
-        user_id=course.user_id,
-    )
-    db.add(db_course)
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(email=user.email, role=user.role)
+    db.add(db_user)
     db.commit()
-    db.refresh(db_course)
-    return db_course
+    db.refresh(db_user)
+    return db_user
 
-def delete_course_by_id(db: Session, course_id: int):
-    query = db.query(Course).get(course_id)
+
+def delete_user_by_id(db: Session, user_id: int):
+    query = db.query(User).get(user_id)
     db.delete(query)
     db.commit()
     db.close()
     return None
 
 
-def update_course_by_id(db:Session,course_id, course: CourseUpdate):
-    db_course = db.query(Course).get(course_id)
-    course_data = course.dict(exclude_unset=True)
-    for key, value in course_data.items():
-        setattr(db_course, key, value)
-    db.add(db_course)
+def update_user_by_id(db:Session,user_id,user: UserUpdate):
+    db_user = db.query(User).get(user_id)
+    user_data = user.dict(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+    db.add(db_user)
     db.commit()
-    db.refresh(db_course)
+    db.refresh(db_user)
     db.close()
-    return db_course
+    return db_user
