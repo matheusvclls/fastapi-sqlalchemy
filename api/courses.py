cc71e3a59173fbf0a03bf9609a28abf5521253e0
Matheus <matheusvclls97@gmail.com>
2023-01-12 10:47:39 -0300

    feat: call CRUD functions in the api routes

--- api/courses.py
+++ api/courses.py
@@ -1,24 +1,51 @@
-from typing import Optional, List
+from typing import List
+
 import fastapi
-from pydantic import BaseModel
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from db.db_setup import get_db
+from schemas.course import Course, CourseCreate, CourseUpdate
+from crud.courses import get_course, get_courses, create_course,delete_course_by_id, update_course_by_id
 
 router = fastapi.APIRouter()
 
-courses = []
 
-
-
-@router.get("/courses")
-async def get_courses():
+@router.get("/courses", response_model=List[Course])
+async def read_courses(db: Session = Depends(get_db)):
+    courses = get_courses(db=db)
     return courses
 
 
-@router.post("/courses")
-async def create_course():
-    courses.append()
-    return "Success"
+@router.post("/courses", response_model=Course)
+async def create_new_course(
+    course: CourseCreate, db: Session = Depends(get_db)
+):
+    return create_course(db=db, course=course)
 
 
-@router.get("/courses/{id}")
-async def get_course(id: int):
-    return { "course": courses[id] }
+@router.get("/courses/{course_id}")
+async def read_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = get_course(db=db, course_id=course_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return db_course
+
+@router.delete('/courses/{course_id}')
+def delete_course(course_id: int, db: Session = Depends(get_db)):
+    db_user = get_course(db=db, course_id=course_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    delete_course_by_id(db=db,course_id=course_id)
+    return f"Course {course_id} deleted successfully"
+
+
+@router.patch("/courses/{course_id}", response_model=Course)
+async def update_user(course_id: int, course: CourseUpdate, db: Session = Depends(get_db)):
+    db_course = await get_course(db=db, course_id=course_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    
+    db_course = update_course_by_id(db=db,course_id=course_id, course=course)
+    
+    return db_course
