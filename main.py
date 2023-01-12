a562752eaff6cd55f0a278853bedd2e5cb65bbef
Matheus <matheusvclls97@gmail.com>
2023-01-12 10:53:40 -0300

    feat: create all tables of the project

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from api import users,courses
+
+from api import users, courses
+from db.db_setup import engine
+from models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
